12a3e93df5f6717c73287b923ce4e7977cac41fe
Guillaume <dussartguillaume@orange.fr>
2017-08-24 21:03:13 +0200

    add login button*

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
-
+export const firebaseConfig = {
+    apiKey: "AIzaSyD9mtQ8vsTFqp-tdVLBZgvk83RicYlTJHg",
+    authDomain: "angularfirebase-1cf3d.firebaseapp.com",
+    databaseURL: "https://angularfirebase-1cf3d.firebaseio.com",
+    projectId: "angularfirebase-1cf3d",
+    storageBucket: "angularfirebase-1cf3d.appspot.com",
+    messagingSenderId: "209746225879"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      FormsModule,
+      HttpModule,
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireDatabaseModule,
+      AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
